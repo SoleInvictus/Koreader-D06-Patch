e9145b1e70e408ade41b0acb30953458eaca4fea
SoleInvictus <39067872+SoleInvictus@users.noreply.github.com>
2026-09-21 21:34:21 -0700

    Add copyright and license information
    Added copyright and license information to the file.

--- d06keys.lua
+++ d06keys.lua
@@ -1,4 +1,19 @@
 -- d06keys.lua
+-- Copyright (C) 2026 SoleInvictus
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
 -- Turns the D06 ring's "swipe" buttons into real key presses on a Kobo.
 --
 -- Each ring button sends a mouse drag in one direction (left/right/up/down).
